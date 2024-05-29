25cae5782b58b4c9dcb4d07cb24dab9e58214a05
Eli Hansen <15258789+emh68@users.noreply.github.com>
2024-05-29 15:11:21 -0600

    Initial commit

---
+++ Sandbox/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+
+int points = 10;
+
+
+char ConvertPointsToGrade(int points)
+{
+    string result;
+    switch (points)
+    {
+        case 10:
+        case 9:
+            return 'A';
+
+        case 8:
+        case 7:
+        case 6:
+            return 'B';
+
+        case 5:
+        case 4:
+        case 3:
+            return 'C';
+
+        case 2:
+        case 1:
+            return 'D';
+
+        case 0:
+            return 'E';
+
+        default:
+            return '!';
+    }
+}
