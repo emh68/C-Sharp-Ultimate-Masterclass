00beafd7116a8f6517ac90c658832b2108a64813
Eli Hansen <15258789+emh68@users.noreply.github.com>
2024-06-05 14:19:13 -0600

    Initial commit

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,35 +1,35 @@
-﻿Console.WriteLine("Hello!");
-Console.WriteLine("What do you want to do?");
-Console.WriteLine("[S]ee all TODOs");
-Console.WriteLine("[A]dd a TODO");
-Console.WriteLine("[R]emove a TODO");
-Console.WriteLine("[E]xit");
+﻿// Console.WriteLine("Hello!");
+// Console.WriteLine("What do you want to do?");
+// Console.WriteLine("[S]ee all TODOs");
+// Console.WriteLine("[A]dd a TODO");
+// Console.WriteLine("[R]emove a TODO");
+// Console.WriteLine("[E]xit");
 
-string userChoice = Console.ReadLine();
-if(userChoice == "S")
-{
-    PrintSelectedOption("See all TODOs");
-}
+// string userChoice = Console.ReadLine();
+// if(userChoice == "S")
+// {
+//     PrintSelectedOption("See all TODOs");
+// }
 
-else if (userChoice == "A")
-{
-    PrintSelectedOption("Add a TODO");
-}
-    
-else if (userChoice == "R")
-{
-    PrintSelectedOption("Remove a TODO");
-}
+// else if (userChoice == "A")
+// {
+//     PrintSelectedOption("Add a TODO");
+// }
 
-else if (userChoice == "E")
-{
-    PrintSelectedOption("Exit");
-}
+// else if (userChoice == "R")
+// {
+//     PrintSelectedOption("Remove a TODO");
+// }
 
-Console.ReadKey();
+// else if (userChoice == "E")
+// {
+//     PrintSelectedOption("Exit");
+// }
 
-void PrintSelectedOption(string selecteOption)
-{
-    Console.WriteLine($"Selected option: {selecteOption}");
-}
+// Console.ReadKey();
+
+// void PrintSelectedOption(string selecteOption)
+// {
+//     Console.WriteLine($"Selected option: {selecteOption}");
+// }
 
